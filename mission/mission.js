e243ba5321615942be72f88756965f7ab3ea13e1
jaredsrice <jsrice1999@hotmail.com>
2025-09-24 14:58:54 -0600

    Finished Mission Statement pt2

---
+++ mission/mission.js
@@ -0,0 +1,22 @@
+const themeSelector = document.querySelector('#theme');
+
+const body = document.body;
+const logo = document.querySelector('.logo');
+
+function changeTheme() {
+
+    if (themeSelector.value === 'dark')
+    {
+        body.classList.add('dark');
+        logo.src = 'byui-logo_white.png';
+    }
+    else
+    {
+        body.classList.remove('dark');
+        logo.src = 'byui-logo_blue.webp';
+    }
+}
+
+themeSelector.addEventListener('change', changeTheme);
+
+changeTheme();
