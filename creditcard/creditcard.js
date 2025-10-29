0030ba9fdc745794e5fbc42b5b6c16d77afd13fb
jaredsrice <jsrice1999@hotmail.com>
2025-10-28 22:18:59 -0600

    fixed titles

--- creditcard/creditcard.js
+++ creditcard/creditcard.js
@@ -9,28 +9,28 @@ function validateForm(event) {
   var year = document.getElementById("exp-year");
   var cvc = document.getElementById("cvc");
 
-  // CARD NUMBER
+  // Card Number
   if (number.value === "") {
     errors.push("Card number is required.");
   } else if (number.value.length < 15 || number.value.length > 16) {
     errors.push("Card number must be between 14 and 16 characters.");
   }
 
-  // CARD HOLDER
+  // Card Holder
   if (holder.value === "") {
     errors.push("Card holder is required.");
   } else if (holder.value.length < 2 || holder.value.length > 40) {
     errors.push("Card holder must be between 2 and 40 characters.");
   }
 
-  // EXPIRATION MONTH
+  // Expiration Month
   if (month.value === "") {
     errors.push("Expiration month is required.");
   } else if (month.value.length !== 2) {
     errors.push("Expiration month must have 2 digits.");
   }
 
-  // EXPIRATION YEAR
+  // Expiration Year
   if (year.value === "") {
     errors.push("Expiration year is required.");
   } else if (year.value.length !== 2) {
@@ -44,7 +44,7 @@ function validateForm(event) {
     errors.push("CVC must be 3 or 4 digits.");
   }
 
-  // SHOW ERRORS
+  // Show Errors
   var errorBox = document.querySelector(".errors");
   if (errors.length > 0) {
     var message = "";
